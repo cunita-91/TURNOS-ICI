2e50d01f91c80c7e58607099d3cfbc6252081986
cunita-91 <combet606@gmail.com>
2023-11-15 22:09:53 -0300

    Crear_tablaPROF

---
+++ INSERT INTO Turnero.sql
@@ -0,0 +1,6 @@
+INSERT INTO Turnero.Profesionales(Id_prof, Apellido_prof, Nombre_prof,Profesion, Dia_atencion) VALUES
+(NULL, "Zayat", "Andrea", "Cardiologa", "Miercoles"), (NULL,"Vargas","Martina","Cardiologa","Lunes y Martes"),
+(NULL, "Martinez", "Mercedes", "Cardiologa", "Lunes"), (NULL, "Turbay", "Susana", "Cardiologa", "Martes"),
+(NULL, "Ovand", "Marcela", "Cardiologa", "Miercoles"), (NULL, "Altman", "Rocio", "Cardiologa", "Jueves"), 
+(NULL, "Quin", "Estela", "Cardiologa", "Jueves y Viernes"), (NULL, "Zerpa", "Victoria", "Cardiologa", "Jueves"),
+(NULL, "Salvi", "Esteban", "Electrofisiologo", "Viernes");
